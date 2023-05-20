d150cb006e7dd21203816a6f28267d8ba30020e6
Leng Yang <lengyang@Lengs-Mac-Studio.local>
2023-05-20 01:10:37 -0500

    more wording fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import { apiSlice } from './slices/ApiSlice';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
     reducer: {
